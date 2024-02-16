cc9aa5501d5f85e4d976e87fddb81281b8883b2f
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-16 15:20:13 +0100

    endpoint recipes/id

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import logger from "morgan";
 import "dotenv/config";
 import ingredientsRouter from "./routes/ingredientsRouter.js";
+import recipesRouter from "./routes/recipesRouter.js";
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(logger(formatsLogger));
 app.use("/ingredients", ingredientsRouter);
+app.use("/recipes", recipesRouter);
 
 app.use((req, res, next) => {
   res.status(404).send("404 Not Found");
