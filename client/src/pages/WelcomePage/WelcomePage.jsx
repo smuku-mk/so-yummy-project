8d2dad31429e794191f5f8388904c92e84ae37d1
mika <dominikanajda@gmail.com>
2024-02-18 15:20:16 +0100

    register-page
    - register, signin i welcome page
    - stylowanie niekompletne (bez ikon i zdjęć) wszystko będzie się rozwalać, czekam na folder z obrazkami, bo ktoś miał robić

---
+++ client/src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { Container, Title, Text, Register, Signin } from "./WelcomePage.styled";
+
+const WelcomePage = () => {
+  return (
+    <Container>
+      <Title>Welcome to the app!</Title>
+      <Text>
+        This app offers more than just a collection of recipes - it is designed
+        to be your very own digital cookbook. You can easily save and retrieve
+        your own recipes at any time.
+      </Text>
+      <Register to={"/register"}>Registration</Register>
+      <Signin to={"/signin"}>Sign in</Signin>
+    </Container>
+  );
+};
+
+export default WelcomePage;
---
+++ client/src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div``;
+
+export const Title = styled.h3`
+  font-size: 24px;
+  letter-spacing: -0.02em;
+  color: var(--fontLight);
+  font-weight: 600;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 28px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  letter-spacing: -0.02em;
+  color: var(--fontLight);
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+export const Register = styled(Link)`
+  color: var(--fontLight);
+  padding: 12px 24px;
+  background-color: var(--brand);
+  font-size: 14px;
+  border-radius: 24px 44px;
+
+  &:focus,
+  :hover {
+    background-color: transparent;
+  }
+
+  @media (min-width: 768px) {
+    padding: 22px 44px;
+    font-size: 16px;
+  }
+`;
+
+export const Signin = styled(Link)`
+  color: var(--fontLight);
+  padding: 12px 24px;
+  background-color: transparent;
+  font-size: 14px;
+  border-radius: 24px 44px;
+  background-color: var(--brand);
+
+  &:focus,
+  :hover {
+    background-color: var(--brand);
+  }
+
+  @media (min-width: 768px) {
+    padding: 22px 44px;
+    font-size: 16px;
+  }
+`;
