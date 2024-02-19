94b00150fbf67df8a2880c896f7cf33ba121698b
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-19 11:38:45 +0100

    getRecipeByTitle

--- server/routes/recipesRouter.js
+++ server/routes/recipesRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getRecipeById } from "../controllers/getRecipeById.js";
+import { getRecipeByTitle } from "../controllers/getRecipeByTitle.js";
 
 const recipesRouter = express.Router();
 
+recipesRouter.get("/search", getRecipeByTitle);
 recipesRouter.get("/:id", getRecipeById);
 
 export default recipesRouter;
