973b00382671a839af4f68517e2bc201453cabaa
Mateusz Kubiak <smuku.mk@gmail.com>
2024-02-21 14:30:50 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles";
 import { lightTheme, darkTheme } from "./styles/theme";
 import { App } from "./components";
-import store from './redux/store';
+import store from "./redux/store";
 
 const Root = () => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
-   const toggleTheme = () => {
-  setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
-};
-   
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
     <React.StrictMode>
       <Provider store={store}>
-        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> 
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
           <GlobalStyles />
-          <App  toggleTheme={toggleTheme}  />
-         </ThemeProvider> 
+          <App toggleTheme={toggleTheme} />
+        </ThemeProvider>
       </Provider>
     </React.StrictMode>
   );
