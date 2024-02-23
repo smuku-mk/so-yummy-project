c385654bbf67f0992c6702d3a4394773b153a9d6
Adrian Nastał <adriannastal9@gmail.com>
2024-02-23 21:23:55 +0100

    poprawione kolory

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SharedLayout } from "./SharedLayout";
 import { lazy } from "react";
 
-const lazyLoad = (page) => lazy(() => import("../pages").then((module) => ({ default: module[page] })));
+const lazyLoad = (page) =>
+  lazy(() => import("../pages").then((module) => ({ default: module[page] })));
 const WelcomePage = lazyLoad("WelcomePage");
 
 export const App = () => {
