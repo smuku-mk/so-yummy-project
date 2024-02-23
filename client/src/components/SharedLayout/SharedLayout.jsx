06fd5a1b68f3476e678a183033fce822c31e8c4b
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-23 11:56:53 +0100

    footer position fix

--- client/src/components/SharedLayout/SharedLayout.jsx
+++ client/src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,16 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Container } from "./SharedLayout.styled";
+import { Container, MainContent } from "./SharedLayout.styled";
 // import { Header } from "../Header";
 import { Footer } from "../Footer";
 
-export const SharedLayout = () => {
+export const SharedLayout = ({children}) => {
   return (
     <Container>
       {/* <Header /> */}
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
+        <MainContent>{children}</MainContent>
       </Suspense>
       <Footer />
     </Container>
--- client/src/components/SharedLayout/SharedLayout.styled.jsx
+++ client/src/components/SharedLayout/SharedLayout.styled.jsx
@@ -5,6 +5,9 @@ export const Container = styled.div`
   max-width: 1440px;
   margin: 0 auto;
   padding: 0 16px;
+  display: flex; 
+  flex-direction: column;
+  min-height: 100vh;
 
   @media screen and (min-width: 768px) {
     padding: 0 32px;
@@ -13,4 +16,8 @@ export const Container = styled.div`
   @media screen and (min-width: 1440px) {
     padding: 0 100px;
   }
+`;
+
+export const MainContent = styled.main`
+  flex-grow: 1;
 `;
