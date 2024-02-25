4425ee5e6bb6139772cb4efbd84a545d8c3f2c42
MalgorzataKuszewska <mkuszewska242@gmail.com>
2024-02-25 19:53:07 +0100

    Update categories page

---
+++ client/src/pages/CategoriesPage/CategoriesPage.jsx
@@ -0,0 +1,12 @@
+import { CategoriesList } from "../../components/CategoriesList/";
+import { CategoriesPageStyled } from "./CategoriesPage.styled";
+
+const CategoriesList = () => {
+  return (
+    <CategoriesPageStyled>
+      <CategoriesList />
+    </CategoriesPageStyled>
+  );
+};
+
+export default CategoriesList;
---
+++ client/src/pages/CategoriesPage/CategoriesPage.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const CategoriesPageStyled = styled.section`
+  @media (min-width: 768px) {
+  }
+  @media (min-width: 1280px) {
+  }
+`;
