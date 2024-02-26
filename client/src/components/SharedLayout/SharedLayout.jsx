c629e50c6d06cc9568e8adbc84d935be908a4ea3
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-26 13:48:11 +0100

    auth fix

--- client/src/components/SharedLayout/SharedLayout.jsx
+++ client/src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,18 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { currentUser } from "../../redux/auth/operations";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export const SharedLayoutPrivate = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(currentUser());
   }, [dispatch]);
 
-  console.log("isLoggedIn", isLoggedIn);
 
   return (
     <>
