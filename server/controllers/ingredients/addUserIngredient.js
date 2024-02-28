020a9e754df5baacd99cc04fffde56aa32b494e3
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-28 20:49:10 +0100

    addUserIngredient endpoint

---
+++ server/controllers/ingredients/addUserIngredient.js
@@ -0,0 +1,10 @@
+import User from '../../database/models/userSchema.js';
+
+export const addUserIngredient = async (req, res) => {
+    const { userId } = req.params;
+    const ingredient = req.body;
+
+    const user = await User.findByIdAndUpdate(userId, { $push: { shoppingList: ingredient } }, { new: true });
+
+    return res.json(user);
+};
--- server/routes/shoppingListRouter.js
+++ server/routes/shoppingListRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { listIngredients } from '../controllers/ingredients/listIngredients.js'
-import { addIngredients } from "../controllers/ingredients/addIngredients.js";  
+import { addUserIngredient } from "../controllers/ingredients/addUserIngredient.js";
 import { removeIngredients } from "../controllers/ingredients/removeIngredients.js"; 
 import { catchErr } from "../middlewares/catchErr.js";
 
 export const shoppingListRouter = express.Router();
 
-shoppingListRouter.get("/", catchErr(listIngredients));
-shoppingListRouter.post("/add", catchErr(addIngredients));
-shoppingListRouter.delete("/remove/:productId", catchErr(removeIngredients));
+//shoppingListRouter.get("/:userId", catchErr());
+shoppingListRouter.post("/:userId", catchErr(addUserIngredient));
+//shoppingListRouter.delete("/:userId", catchErr(removeIngredients));
