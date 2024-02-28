61ca772716937c67316c6036c3375316adc9318c
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-28 23:20:02 +0100

    fix

--- server/routes/shoppingListRouter.js
+++ server/routes/shoppingListRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addUserIngredient } from "../controllers/ingredients/addUserIngredient.js";
-import { removeIngredients } from "../controllers/ingredients/removeIngredients.js"; 
+import { removeUserIngredients } from "../controllers/ingredients/removeUserIngredients.js";
+import { listUserIngredients } from "../controllers/ingredients/listUserIngredients.js";
 import { catchErr } from "../middlewares/catchErr.js";
 
 export const shoppingListRouter = express.Router();
 
-//shoppingListRouter.get("/:userId", catchErr());
+shoppingListRouter.get("/:userId", catchErr(listUserIngredients));
 shoppingListRouter.post("/:userId", catchErr(addUserIngredient));
-//shoppingListRouter.delete("/:userId", catchErr(removeIngredients));
+shoppingListRouter.delete("/:userId", catchErr(removeUserIngredients));
