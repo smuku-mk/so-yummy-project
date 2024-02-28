273f6148c9636057adbd188a5edf012982de152e
Mateusz Kubiak <smuku.mk@gmail.com>
2024-02-28 11:23:58 +0100

    Update FollowUs.jsx

--- client/src/components/Footer/FollowUs/FollowUs.jsx
+++ client/src/components/Footer/FollowUs/FollowUs.jsx
@@ -1,40 +1,34 @@
-import React from 'react';
-import sprites from '../../../images/sprites.svg';
-import {
-  SocialsContainer,
-  SocialsList,
-  SocialMedia,
-  SocialLink,
-  SocialIcon,
-} from './FollowUs.styled';
+import React from "react";
+import sprites from "../../../images/sprites.svg";
+import { SocialsContainer, SocialsList, SocialMedia, SocialLink, SocialIcon } from "./FollowUs.styled";
 
 export const FollowUs = () => {
   return (
     <SocialsContainer>
       <SocialsList>
         <SocialMedia>
-          <SocialLink href="https://www.facebook.com/">
+          <SocialLink href="https://www.facebook.com/" target="_blank">
             <SocialIcon>
               <use xlinkHref={`${sprites}#icon-facebook`} />
             </SocialIcon>
           </SocialLink>
         </SocialMedia>
         <SocialMedia>
-          <SocialLink href="https://www.youtube.com/">
+          <SocialLink href="https://www.youtube.com/" target="_blank">
             <SocialIcon>
               <use xlinkHref={`${sprites}#icon-youtube`} />
             </SocialIcon>
           </SocialLink>
         </SocialMedia>
         <SocialMedia>
-          <SocialLink href="https://twitter.com/">
+          <SocialLink href="https://twitter.com/" target="_blank">
             <SocialIcon>
               <use xlinkHref={`${sprites}#icon-twitter`} />
             </SocialIcon>
           </SocialLink>
         </SocialMedia>
         <SocialMedia>
-          <SocialLink href="https://www.instagram.com/">
+          <SocialLink href="https://www.instagram.com/" target="_blank">
             <SocialIcon>
               <use xlinkHref={`${sprites}#icon-instagram`} />
             </SocialIcon>
