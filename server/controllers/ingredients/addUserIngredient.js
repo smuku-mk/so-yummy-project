b1082202d0197b5adaf92c732986cac3aa817354
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-28 22:00:47 +0100

    Update addUserIngredient.js

--- server/controllers/ingredients/addUserIngredient.js
+++ server/controllers/ingredients/addUserIngredient.js
@@ -4,7 +4,7 @@ export const addUserIngredient = async (req, res) => {
     const { userId } = req.params;
     const ingredient = req.body;
 
-    const user = await User.findByIdAndUpdate(userId, { $push: { shoppingList: ingredient } }, { new: true });
+    const user = await User.findByIdAndUpdate(userId, { $push: { shoppingList: ingredient.ttl } }, { new: true });
 
     return res.json(user);
 };
