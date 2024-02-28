94afb498e746b3a421b19b428524f17ac5bd3c5c
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2024-02-28 17:09:39 +0100

    fix ownRecipes

--- server/controllers/ownRecipes/getUserRecipes.js
+++ server/controllers/ownRecipes/getUserRecipes.js
@@ -2,6 +2,6 @@ import Recipe from "../../database/models/recipesSchema.js";
 
 export const getUserRecipes = async (req, res) => {
 
-    return res.status(200).json(await Recipe.find({ owner: req.user.id }));
+    return res.status(200).json(await Recipe.find({ owner: req.params.userId }));
     
 };
