ba43b7239aa4149383833aa927ee6c6d1ef574df
Mateusz Kubiak <smuku.mk@gmail.com>
2024-03-08 14:11:31 +0100

    Update RecipeList.jsx

--- client/src/components/Categories/RecipeList/RecipeList.jsx
+++ client/src/components/Categories/RecipeList/RecipeList.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRecipes } from '../../../redux/categories/operations'
-import { recipes, currentCategory } from '../../../redux/categories/selectors'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRecipes } from "../../../redux/categories/operations";
+import { recipes, currentCategory } from "../../../redux/categories/selectors";
 import { Container, RecipesList, PictureDiv, PictureImg, PictureDescription } from "./RecipeList.styled";
 
 export const RecipeList = () => {
   const dispatch = useDispatch();
-  const recipesList = useSelector(recipes); 
+  const recipesList = useSelector(recipes);
   const category = useSelector(currentCategory);
 
   useEffect(() => {
@@ -19,15 +19,15 @@ export const RecipeList = () => {
         {recipesList &&
           recipesList.map((recipe, index) => (
             <li key={index}>
-                <PictureDiv>
-                  <PictureImg src={recipe.thumb} alt={recipe.title} />
-                  <PictureDescription>
-                   <p>{recipe.title}</p>
-                  </PictureDescription>
-                </PictureDiv>
+              <PictureDiv>
+                <PictureImg src={recipe.thumb} alt={recipe.title} />
+                <PictureDescription>
+                  <p>{recipe.title}</p>
+                </PictureDescription>
+              </PictureDiv>
             </li>
           ))}
       </RecipesList>
     </Container>
   );
- }
+};
